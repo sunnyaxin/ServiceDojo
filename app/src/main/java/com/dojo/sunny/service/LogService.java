417c9aae7e9b7d8d2b03adadb360b9b444e985f2
Yaxin Wang <yxwang@thoughtworks.com>
2018-05-27 23:50:17 +0800

    add toast to test service lifecycle

--- app/src/main/java/com/dojo/sunny/service/LogService.java
+++ app/src/main/java/com/dojo/sunny/service/LogService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class LogService extends Service {
 
@@ -12,12 +13,14 @@ public class LogService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Toast.makeText(this, "on create", Toast.LENGTH_SHORT).show();
         Log.e(TAG, ": on create");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e(TAG, ": on start command");
+        Toast.makeText(this, "on start command", Toast.LENGTH_SHORT).show();
         String str = intent.getExtras().getString("service_string");
         Log.e(TAG, str);
         return super.onStartCommand(intent, flags, startId);
@@ -25,12 +28,14 @@ public class LogService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "on bind", Toast.LENGTH_SHORT).show();
         Log.e(TAG, ": on bind");
         return null;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "on un bind", Toast.LENGTH_SHORT).show();
         Log.e(TAG, ": on un bind");
         return super.onUnbind(intent);
     }
@@ -38,6 +43,7 @@ public class LogService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Toast.makeText(this, "on destroy", Toast.LENGTH_SHORT).show();
         Log.e(TAG, ": on destroy");
     }
 }
