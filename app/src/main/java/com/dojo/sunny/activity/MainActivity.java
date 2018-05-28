cfecd9536805764c503bde6b86e648a1cd421a69
Yaxin Wang <yxwang@thoughtworks.com>
2018-05-28 16:00:27 +0800

    add messenger reply message from service to activity

--- app/src/main/java/com/dojo/sunny/activity/MainActivity.java
+++ app/src/main/java/com/dojo/sunny/activity/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dojo.sunny.R;
@@ -20,7 +22,10 @@ public class MainActivity extends Activity implements Button.OnClickListener {
 
     private boolean bound = false;
 
-    private Messenger messenger;
+    private Messenger remoteMessenger;
+    private Messenger localMessenger = new Messenger(new LocalHandler());
+
+    private TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends Activity implements Button.OnClickListener {
 
         Button bind = findViewById(R.id.bind_service);
         Button unbind = findViewById(R.id.unbind_service);
+        question = findViewById(R.id.question);
 
         bind.setOnClickListener(this);
         unbind.setOnClickListener(this);
@@ -68,23 +74,42 @@ public class MainActivity extends Activity implements Button.OnClickListener {
         @Override
         public void onServiceConnected(ComponentName name, IBinder iBinder) {
             bound = true;
-            messenger = new Messenger(iBinder);
+            remoteMessenger = new Messenger(iBinder);
             answerQuestion(MessengerService.MSG_ANSWER_NAME);
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
             bound = false;
-            messenger = null;
+            remoteMessenger = null;
         }
     };
 
     private void answerQuestion(int what) {
-        Message message = Message.obtain(null, what, 0, 0);
+        Message message = Message.obtain();
+        message.what = what;
+        message.replyTo = localMessenger;
         try {
-            messenger.send(message);
+            remoteMessenger.send(message);
         } catch (RemoteException e) {
             e.printStackTrace();
+        }
+    }
+
+    class LocalHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            String result = msg.getData().getString("result");
+            switch (msg.what) {
+                case MessengerService.MSG_ANSWER_NAME:
+                    question.setText(result);
+                    break;
+                case MessengerService.MSG_QUESTION:
+                    question.setText(result);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
         }
     }
 }
--- app/src/main/java/com/dojo/sunny/service/MessengerService.java
+++ app/src/main/java/com/dojo/sunny/service/MessengerService.java
@@ -2,10 +2,12 @@ package com.dojo.sunny.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
@@ -20,12 +22,30 @@ public class MessengerService extends Service {
     private class IncomingHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
+            Message newMsg = Message.obtain();
+            Bundle bundle = new Bundle();
             switch (msg.what) {
                 case MSG_ANSWER_NAME:
-                    Toast.makeText(MessengerService.this, answerName(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MessengerService.this, "service: " + answerName(), Toast.LENGTH_SHORT).show();
+                    newMsg.what = MSG_ANSWER_NAME;
+                    bundle.putString("result", answerName());
+                    newMsg.setData(bundle);
+                    try {
+                        msg.replyTo.send(newMsg);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case MSG_QUESTION:
-                    Toast.makeText(MessengerService.this, question(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MessengerService.this, "service: " + question(), Toast.LENGTH_SHORT).show();
+                    newMsg.what = MSG_QUESTION;
+                    bundle.putString("result", question());
+                    newMsg.setData(bundle);
+                    try {
+                        msg.replyTo.send(newMsg);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     super.handleMessage(msg);
